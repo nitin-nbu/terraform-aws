708130488c6c6db05268391a11a108d8d7dc0472
Nitin <nitin.nbu@gmail.com>
2023-01-10 14:36:41 -0600

    Added Var and provider

--- first-step/01-instance.tf
+++ first-step/01-instance.tf
@@ -1,10 +1,4 @@
-provider "aws" {
-  access_key = "AKIAYVKUSBIL6ZXLLJF6"
-  secret_key = "tuqtTUzIdDxwuR1Zu9OQTRPXg4shKaoFPxyygISJ"
-  region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
-    ami = "ami-06878d265978313ca"
+    ami = "${lookup(var.AMIS, var.AWS_REGION)}"
     instance_type = "t2.micro"
 }   
---
+++ first-step/02.vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+    default = "us-east-1"  
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-west-1 = "ami-0454207e5367abf01"
+  }
+}
